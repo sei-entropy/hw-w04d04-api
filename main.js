7d0d195065fe68822fb2f138b77edca668516d7b
bader_mazro <bader.mazro@gmail.com>
2020-02-12 19:27:42 +0300

    click button to show json images

--- main.js
+++ main.js
@@ -2,3 +2,31 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+// https://rickandmortyapi.com/api/character/
+//const items = document.querySelector(". item")
+const image = document.querySelectorAll(".charatarOne")
+const paragaraph = document.querySelectorAll(".name")
+const btn = document.querySelector("#random")
+axios({
+        url: 'https://rickandmortyapi.com/api/character/',
+        method: 'get'
+    })
+    .then(response => {
+        // code for if the request succeeds
+        const charactarList = function () {
+            for (let i = 0; i < 5; i++) {
+                console.log(response.data.results[i].name);
+                console.log(response.data.results[i].image)
+
+                paragaraph[i].innerText = response.data.results[i].name
+                image[i].setAttribute('src', response.data.results[i].image)
+            }
+        }
+        btn.addEventListener('click', charactarList)
+    })
+
+    .catch(error => {
+        // code for if the request fails
+        console.log(error);
+    });
