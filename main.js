817474832e5f45f7e48aa4e957a23216692720df
moh1415 <moh14151415@gmail.com>
2020-02-12 23:48:11 +0300

    homework for api

--- main.js
+++ main.js
@@ -2,3 +2,23 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+let content = $(".content");
+function getRandom() {
+    for(let i =0;i<5;i++)
+        {
+    axios({ method: 'get', url: 'https://ghibliapi.herokuapp.com/films/' })
+      .then(res => {
+        
+            console.log(res);
+            const title = res.data[i].title;
+            const description = res.data[i].description;
+            content.prepend(`<h2>${title}</h2><p>${description}</p>`);
+       
+       
+      })
+      .catch(err => {
+        // WHEN FAILED
+        console.log(err);
+      });
+  }
+}
