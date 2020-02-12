8145b8e455deac103b64f6b6b91e8420940e591e
RanenDev <github/RanenDev>
2020-02-12 20:47:37 +0300

    Done homework API

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
+"use strict";
 // Remember your promises
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+
+//API for" rick and morty"
+const apiUrl = "https://rickandmortyapi.com/api/character/";
+//Button for Action
+const button = document.querySelector("button");
+
+
+//Create funcyion
+const getRick = () => {
+//Using loop to get the first 5 characters
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    const charName = document.createElement("h2");
+
+//Add axios
+    axios({
+      method: "get",
+      url: `${apiUrl}${i}`
+    })
+//get image & name
+      .then(res => {
+        img.src = res.data.image;
+        charName.innerText = res.data.name;
+        document.body.appendChild(document.createElement("br"));
+        document.body.appendChild(charName);
+        document.body.appendChild(img);
+      })
+//for Erorr
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+
+//add event listener 
+button.addEventListener("click", getRick);
+
+
+
+
+
+
+btn.addEventListener('click', showCharacters)
