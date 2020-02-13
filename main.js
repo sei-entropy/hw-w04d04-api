3d173b066ee67fca68792fb86cbf7186dcda7a1f
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-02-13 11:57:30 +0300

    Finished homework by doing the rick and morty part

--- main.js
+++ main.js
@@ -2,3 +2,31 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+function fetchRickAndMortyCharacters() {
+    const requestObject = {
+        method: 'get',
+        url: 'https://rickandmortyapi.com/api/character/1,2,3,4,5'
+    };
+    const successfulResponseCallback = (responseObject) => {
+        const imageBoxes = document.querySelectorAll('.characterBox');
+        for (let i = 0; i < imageBoxes.length; i++) {
+            let h4 = document.createElement('h4');
+            let img = document.createElement('img');
+            h4.innerHTML = responseObject.data[i].name;
+            img.src = responseObject.data[i].image;
+            imageBoxes[i].appendChild(h4);
+            imageBoxes[i].appendChild(img);
+        }
+    };
+    const failedResponseCallback = (responseObject) => {
+        console.log(responseObject);
+    };
+    axios(requestObject)
+        .then(successfulResponseCallback)
+        .catch(failedResponseCallback);
+
+    document.querySelector('#random').style.display = 'none';
+}
+
+document.querySelector('#random').addEventListener('click', fetchRickAndMortyCharacters);
