410a606e8fcc56a729ccd9bf8b4b2f2b40da3e6e
Salman Alsuwailem <salman6111@gmail.com>
2020-02-13 09:04:16 +0300

    solve the HW

--- main.js
+++ main.js
@@ -2,3 +2,51 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+
+
+let url1 = "https://rickandmortyapi.com/api/character/[1,2,3,4,5]";
+
+
+
+function namesAndImages() {
+    axios({
+            method: 'get',
+            url: url1
+        })
+        .then(res => {
+            // WHEN SUCCESS
+            console.log(res);
+      
+         const info= res.data
+    
+            for (let i =0; i<= info.length ; i++) {
+            const images = info[i].image;
+            const names = info[i].name;
+            console.log(images);
+            console.log(names);
+             
+
+            const allImg = document.querySelectorAll('.image')
+            allImg[i].src  = images;
+    
+            // }
+            // const allActors = document.querySelectorAll('.actors').innerText
+            // allActors[i]  = names;
+            $(".actors").text(names);
+            // $(".actors").each (function (i){
+            //     text(names);
+            // })
+
+
+            } 
+         
+        })
+        .catch(err => {
+            // WHEN FAILED
+            console.log(err);
+        })
+
+    }
+     
+  
