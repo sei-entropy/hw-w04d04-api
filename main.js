b2ab84be57c7ab41ecaf3386c04af9a77a739d24
maimonah95 <mim95s@hotmail.com>
2020-02-13 07:16:17 +0300

    modify css , hide button after cick

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
-console.log('ooo');
+console.log('TEST');
 //Rick and Morty API
 //this function to create image and name of it
 function getinfo(){
@@ -11,21 +11,21 @@ axios({
      url: 'https://rickandmortyapi.com/api/character/'
  })
  .then(res => {
-         for(let i=1;i<5;i++){
+         for(let i=0;i<5;i++){
              const imgch = res.data.results[i].image;
              const name = res.data.results[i].name;
              //create image and text
              const txt = $("<h4> </h4>").text(name);
              const img = $("<img/>").attr('src', imgch);
             //$('.container').append(txt,img);
-            //create div .its easy to work with div for each response 
+            //create div .
             var div = $('<div/>');
             div.append(txt, img);
             div.css({
-                    // 'border - bottom': '1 px solid black',
-                    // 'border - right': '1 px solid black',
-                 'border':'1px solid black',
-                'width':'30%',
+                     'border-bottom': '2px solid grey',
+                     'border-right': '2px solid grey',
+                // 'display':'flex',
+                'width':'25%',
                 'text-align':'center',
                 // 'content':'" "',
                 // 'clear':'both',
@@ -33,11 +33,14 @@ axios({
             div.appendTo($('.container'));
          console.log("infoooo : " + res.data.results[i].name); 
          }
+         $('#random').hide();
      })
      //if there is an error
      .catch(err => {
          console.log('error ' + err);
      });
+      //hide button after click
+      $('#random').hide ();
 
     }
     //studio ghibli api 
@@ -48,16 +51,21 @@ axios({
                 url: 'https://ghibliapi.herokuapp.com/films/'
             })
             .then(res => {
-                for (let i = 5; i >= 1; i--) {
+                let a = res.data.length;
+                console.log(a);
+                for (let i = a-1; i >= 1; i--) {
                     const titl = res.data[i].title;
                     const des = res.data[i].description;
                     const txt = $("<h1> </h1>").text(titl);
                     const desc = $("<p> </p>").text(des);
-                    $('.container').after(txt, desc, '<br>')
+                    $('.containerf').append(txt, desc, '<br>')
                     console.log("infoooo : " + titl);
                 }
             })
             .catch(err => {
                 console.log('error ' + err);
             });
+            //hide button after click
+            $('#randomf').hide();
     }
+
