aa75b6c5e7d2cae62e33cb4de782ff390a5740d9
robaarishi24 <robaarishi@hotmail.com>
2020-02-13 09:00:26 +0300

    homework done

--- main.js
+++ main.js
@@ -2,3 +2,33 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+const getInfo = function (){
+
+    axios({
+        method: 'get',
+        url:'https://rickandmortyapi.com/api/character/'
+    })
+    
+    .then(res => {
+    
+        for(let i=0 ; i<5 ; i++){
+            const name = document.createElement("p");
+            name.innerText = res.data.results[i].name;
+            const newname = document.querySelector(".container");
+            newname.append(name);
+            const img = document.createElement("img");
+            img.setAttribute("src", res.data.results[i].image);
+            const newImg = document.querySelector(".container");
+            newImg.append(img);
+    
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      }); 
+    
+    
+    
+    
+    
+    } 
