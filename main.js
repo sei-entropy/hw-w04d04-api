0d1967a888d4f0ec160a41b0566faedb49e58310
huda <you@example.com>
2020-02-13 08:51:46 +0300

    hw

--- main.js
+++ main.js
@@ -2,32 +2,6 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 
-
-/* const URL = "https://rickandmortyapi.com/api/character/";
-
-function getcharacters  () {
-
-    for (let i = 0; i <= 5; i++) {
-    const img = document.createElement("img");
-    const Name = document.createElement("h3");
-
-    axios({ url: URL + i, method: "get" })
-
-      .then(res => {
-        img.src = res.data.image;
-        Name.innerText = res.data.name;
-
-        document.body.appendChild(Name);
-        document.body.appendChild(img);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-}; */
-
-
-//.......................................................
 const charactersList = $('.item');
 function getcharacters  () {
 
