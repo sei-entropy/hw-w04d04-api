99d1e779894358106e4fdb435faf2cb4a1b814ab
wejdann <weejdansaud58@hotmail.com>
2020-02-15 15:07:45 +0300

    Wojdan alrasheeedi update

--- main.js
+++ main.js
@@ -3,50 +3,34 @@
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
 
-
-const API_URL = 'https://rickandmortyapi.com/api/'
-//const END_POINT='/character/1,2,3,4,5'
-const END_POINT = '/character'
-const API_KEY = '1,2,3,4,5'
-//let url1 = 'https://rickandmortyapi.com/api//character/1,2,3,4,5';
-//let url1 = API_URL + END_POINT
-
 function getInfo() {
-    for (i=0 ; i<=5 ;i++){
+
     axios({
             method: 'get',
             url: 'https://rickandmortyapi.com/api//character/1,2,3,4,5'
         })
         .then(res => {
+            const container = document.getElementById('container')
 
-            // WHEN SUCCESS
-            console.log(res);
-            //  let randomSrc = res.data.urls.regular;
-            
-            const randomSrc = res.data[i].image
-            const x = document.getElementsByClassName("container");
-            const image=document.createElement('img')
-            image.setAttribute("src", randomSrc);
-            document.body.appendChild(x);
+            for (i = 0; i <= 5; i++) {
+                console.log(res.data[i].image);
 
-            const returnName = res.data[i].name
-            const y = document.getElementsByClassName("container");
-            // const text=document.createElement('p')
-            // document.getElementById("Name").textContent = returnName;
-            var p = document.getElementById("p")
-             p.innerText = p.innerText+returnName;
-             document.body.appendChild(y);
+                const randomSrc = res.data[i].image
+                const newImage = document.createElement('img')
+                newImage.src = randomSrc;
+                container.appendChild(newImage)
 
-           // $('#Name').text(returnName);
-            console.log(returnName);
-           
-            //document.getElementById("Name").textContent = "This is som text";
+                const returnName = res.data[i].name
+                const newName = document.createElement('p')
+                newName.textContent = returnName;
+                container.appendChild(newName)
 
+            }
 
         })
-    
+
         .catch(err => {
 
             console.log(err);
         });
-}}
+}
