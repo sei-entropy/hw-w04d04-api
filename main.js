37308dd2d05c6a1bdb045687c374838f2b0af066
bedouralshuaibi <b.f.alshuaibi@gmail.com>
2020-02-15 19:59:38 +0300

    fixing loop by adding id in div instead of class

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ axios({
     for(let i=0; i< res.data.length;i++){
         title = $("<h3></h3>").addClass("title").text(res.data[i].title)
         description = $("<p></p>").addClass("para").text(res.data[i].description);
-          $('.container').append(title)
-          $('.container').append(description)
+          $('.container').append(title);
+          $('.container').append(description);
     }
     // document.querySelector('#title').innerText =res.data.title;
     // document.querySelector('#des').innerText =res.data.description;
@@ -36,21 +36,29 @@ function getInfo2(){
     .then(res =>{
         console.log(res);
         let name = "";
-        let img ="";
-        let charector = "";
-       
-        for (let i=0; i < res.data.length;i++){
+         let img ="";
+         let charector = "";
+        // 
+        
+        for (let i=0; i < 5;i++){
             console.log(res.data[i].name)
-            charector = $("<div></div>").addClass("charector");
+            charector = $('<div id="div'+ i +'" />');
             name =  $("<h3></h3>").text(res.data[i].name)
             img = $("<img>").attr('src', res.data[i].image);
-        
-              $(".list").append(charector);
-              $(".charector").append(name);
-              $(".charector").append(img);
-               
-           
+            console.log(charector);
+            console.log(name);
+            console.log(img);
+            
+            $(".list").append(charector);
+            $(charector).append(name);
+            $(charector).append(img); 
+            
         }
+            
+             
+              
+        
+        
     })
     .catch(error =>{
         console.log(error);
